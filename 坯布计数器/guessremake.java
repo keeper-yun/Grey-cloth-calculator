a1f63663a584df3fca44d497d4067f766568d114
keeper-yun <154521005+keeper-yun@users.noreply.github.com>
2024-09-15 16:27:12 +0800

    Update guessremake.java

--- 坯布计数器/guessremake.java
+++ 坯布计数器/guessremake.java
@@ -1,19 +1,24 @@
 package 坯布计数器;
 
 import java.awt.event.*;
-import java.util.Scanner;
 
-public class guessremake implements ActionListener{
+public class guessremake implements ActionListener {
     guessMVC mvc;
 
-    public void setguessMVC(guessMVC mvc){
-        this.mvc =mvc;
+    public void setguessMVC(guessMVC mvc) {
+        this.mvc = mvc;
     }
+
     public void actionPerformed(ActionEvent e) {
-
         try {
-
+            // 清空文本框内容
             mvc.text.setText("");
+
+            // 重置 guess 对象的 sum 和 order
+            mvc.guess.sum = 0.0;
+            for (int i = 0; i < mvc.guess.order.length; i++) {
+                mvc.guess.order[i] = 0.0;
+            }
 
         } catch (Exception exp) {
             exp.printStackTrace();
